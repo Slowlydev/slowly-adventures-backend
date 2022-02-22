fe7e4fdb59f74da232da61e8fd4d3593ce73d2ed
Slowlydev <slowly.developers@gmail.com>
2022-02-22 21:56:21 +0100

    feat: clothing controller modules service

---
+++ src/clothing/clothing.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Clothing } from "./clothing.entity";
+import { ClothingService } from "./clothing.service";
+
+@Controller("/clothing")
+export class ClothingController {
+  constructor(private readonly clothingService: ClothingService) { }
+
+  @Get()
+  async getClothings(): Promise<Clothing[]> {
+    return this.clothingService.findAllClothings();
+  }
+
+  @Get(":id")
+  async getClothing(@Param("id") id: Clothing["id"]): Promise<Clothing> {
+    return this.clothingService.findOneClothing(id);
+  }
+
+  @Post()
+  async postClothing(@Body() request: Clothing): Promise<Clothing> {
+    return this.clothingService.createClothing(request);
+  }
+
+  @Put()
+  async putClothing(@Body() request: Clothing): Promise<Clothing> {
+    return this.clothingService.updateClothing(request);
+  }
+
+  @Delete(":id")
+  async deleteClothing(@Param("id") id: Clothing["id"]): Promise<Clothing> {
+    return this.clothingService.removeOneClothing(id);
+  }
+
+  @Delete()
+  async deleteClothings(): Promise<Clothing[]> {
+    return this.clothingService.removeAllClothings();
+  }
+}
