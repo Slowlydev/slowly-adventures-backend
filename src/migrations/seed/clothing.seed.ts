1c4b81e5795fa557a61a2901615337d1e9b69203
Slowlydev <slowly.ddevelopers@gmail.com>
2022-03-07 13:32:01 +0100

    updated: completed seeds

--- src/migrations/seed/clothing.seed.ts
+++ src/migrations/seed/clothing.seed.ts
@@ -24,4 +24,49 @@ export const clothings: Clothing[] = [
         protection: 100,
         rarityID: 5,
     },
+
+    {
+        name: "AIR FORCE 1",
+        description: "its worn by every mf on earth",
+        protection: 20,
+        rarityID: 1,
+    },
+    {
+        name: "Infinity Gauntlet",
+        description: "only worn by the best of the best",
+        protection: 200,
+        rarityID: 10,
+    },
+    {
+        name: "ApoRed Hoodie",
+        description: "der aller echte",
+        protection: 110,
+        rarityID: 6,
+    },
+    {
+        name: "Carlo Colucci knit sweater",
+        description: "you have too much money",
+        protection: 20,
+        rarityID: 8,
+    },
+    {
+        name: "Jordan 1",
+        description: "you drippy",
+        protection: 90,
+        rarityID: 4,
+    },
+    {
+        name: "Tommy Hilfiger sweatshirt",
+        description: "ok, I guess",
+        protection: 40,
+        rarityID: 2,
+    },
+    {
+        name: "Versace Jeans",
+        description: "have you lost your mind, you have too much money",
+        protection: 80,
+        rarityID: 8,
+    },
+
+
 ]
